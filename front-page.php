34e1f24f7ffbcd14b5c97f40ed469512e2c21bdf
Khagendra Lama <khagendralama2@gmail.com>
2024-09-11 22:08:06 +0545

    Hero section designed

--- front-page.php
+++ front-page.php
@@ -2,6 +2,21 @@
 get_header();
 ?>
 
-<h1>The Points World</h1>
+<!-- Hero Section Start -->
+<section>
+    <div class="tpw-container tpw-flex tpw-mx-auto tpw-items-center tpw-justify-between">
+        <div class="">
+            <h1 class="tpw-max-w-xl">Unlock the Secrets to Traveling Anywhere for Minimal Cost!</h1>
+            <p class="tpw-max-w-lg tpw-mt-8 tpw-text-xl tpw-font-light">Join me as I share my personal strategies for earning thousands in cashback and traveling the world on a budget.</p>
+            <a href="#" class="tpw-bg-primary tpw-mt-12 tpw-text-white tpw-px-8 tpw-py-4 tpw-text-lg tpw-rounded hover:tpw-text-white hover:tpw-bg-secondary tpw-inline-block">Sign Up for My Course</a>
+            <br>
+            <a href="#" class="tpw-border-2 tpw-rounded tpw-text-primary hover:tpw-bg-primary hover:tpw-text-white tpw-text-lg tpw-mt-6 tpw-px-8 tpw-py-3 tpw-border-primary tpw-inline-block">Book a 1-Hour Consulting Session</a>
+        </div>
+        <div class="tpw-max-w-xl">
+            <img src="/wp-content/uploads/2024/09/hero-banner-resized.jpg" alt="Travel the world">
+        </div>
+    </div>
+</section>
+<!-- Hero Section End -->
 
 <?php get_footer(); ?>
